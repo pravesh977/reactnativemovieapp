c296913040cb78c0ac6cc5a3a92685e786096a31
Pravesh Shrestha <praveshshrestha@Praveshs-MBP.fios-router.home>
2017-12-28 13:34:55 -0500

    second commit buttons

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { StyleSheet, Text, View } from 'react-native';
+import homemoviespic from './images/homemovies.jpg';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
        <Header/>
+       <Image source={homemoviespic} alt="mainpic" style={{flex: 1, width: '100%'}}/>
        <Footer/>
       </View>
     );
@@ -18,8 +20,9 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableHighlight, Alert, StyleSheet } from 'react-native';
 
 class Footer extends React.Component {
+
+
+    _onPressButton() {
+        Alert.alert('You tapped the button!')
+      }
+    
   render() {
     return (
-    <View>
-        <Text>This is a footer </Text>
+    <View style={styles.container}>
+
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>New</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Search</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Favorites</Text>
+          </View>
+        </TouchableHighlight>
     </View>
     );
   }
 }
+const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'stretch',
+      justifyContent: 'space-around',
+      backgroundColor: 'pink',
+    },
+    button: {
+      alignItems: 'stretch',
+      backgroundColor: '#2196F3'
+    },
+    buttonText: {
+      padding: 20,
+      color: 'white'
+    }
+  })
 
 export default Footer;
